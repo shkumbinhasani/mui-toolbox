b76cce5574b2b125f6d0536d5a1ac72712e19d15
Shkumbin Hasani <sh44223@ubt-uni.net>
2021-12-22 04:49:17 +0100

    Init

---
+++ src/components/Sidebar/Sidebar.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+interface SidebarProps {
+    fullHeight?: boolean
+}
+
+const Sidebar = (props: SidebarProps) => {
+    return <div>ja</div>
+}
+
+export default Sidebar
